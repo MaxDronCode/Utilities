b0af603792502178e2c6219969af57f21f67001a
Max Dron <maxdron@icloud.com>
2024-01-30 17:18:38 +0100

    Add new readInt method with min but no max param

--- src/main/kotlin/utilities.kt
+++ src/main/kotlin/utilities.kt
@@ -1,6 +1,5 @@
 package org.example
 
-package org.example
 
 import java.util.*
 import kotlin.math.round
@@ -235,11 +234,12 @@ fun readInt(pMessageIn: String
  * @param pMax Max accepted value
  * @return outputValue Output value
  */
-fun readInt(pMessageIn: String
-            , pMessageErrorDT: String
-            , pMessageErrorDV: String
-            , pMin: Int
-            , pMax: Int
+fun readInt(
+    pMessageIn: String,
+    pMessageErrorDT: String,
+    pMessageErrorDV: String,
+    pMin: Int,
+    pMax: Int,
 ): Int{
 
     var outputValue: Int = 0
@@ -255,6 +255,45 @@ fun readInt(pMessageIn: String
             outputValue = scan.nextInt()
 
             if (outputValue < pMin || outputValue > pMax){
+                println(YELLOW_BOLD_BRIGHT + "WARNING: " + pMessageErrorDV + RESET)
+                correctDataType = false
+            }
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner with min value but no max
+ * @param pMessageIn - Input message to be shown to the user
+ * @param pMessageErrorDT - Data type error message to be shown to the user
+ * @param pMessageErrorDV - Data value error message to be shown to the user
+ * @param pMin - Min accepted value
+ * @return outputValue Output value
+ * @author Max Dron
+ * @since 30/01/2024
+ *
+ */
+fun readInt(pMessageIn: String,
+            pMessageErrorDT: String,
+            pMessageErrorDV: String,
+            pMin: Int
+): Int{
+    var outputValue: Int = 0
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextInt()
+
+        if (!correctDataType){
+            println(RED_BACKGROUND_BRIGHT + "ERROR: " + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scan.nextInt()
+
+            if (outputValue < pMin){
                 println(YELLOW_BOLD_BRIGHT + "WARNING: " + pMessageErrorDV + RESET)
                 correctDataType = false
             }
